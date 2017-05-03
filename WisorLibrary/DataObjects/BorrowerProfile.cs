f116774f4aae32679fff83021d54155f2c0bf263
Shuky Barbash <shuky.barbash@gmail.com>
2017-05-03 16:57:45 +0300

    calculate the borrowe class from ytlv and pti when not set

--- WisorLibrary/DataObjects/BorrowerProfile.cs
+++ WisorLibrary/DataObjects/BorrowerProfile.cs
@@ -15,56 +15,151 @@ namespace WisorLib
         // General Parameters
         // Local market
         // Israel only
-        private readonly uint[] giniIndexList = new uint[3] { 9500, 6718, 1 }; // income depemdency
+        //private readonly uint[] giniIndexList = new uint[3] { 9500, 6718, 1 }; // income depemdency
+        // TBD: read it from file for all markets
         private readonly double[] ltvRatios = new double[5] { 0.50, 0.60, 0.70, 0.75, 0.80 }; // loan amount / property value (loan to value)
         private readonly double[] ptiRatios = new double[5] { 0.20, 0.25, 0.30, 0.35, 0.40 }; // monthy patment / income (payment to income)
+        private readonly int[,] profileMatrix = new int[5, 5] {
+                                {
+                                    (int)CalculationConstants.borrowerProfiles.BEST,
+                                    (int)CalculationConstants.borrowerProfiles.VERY_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.VERY_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.AVERAGE
+                                },
+                                {
+                                    (int)CalculationConstants.borrowerProfiles.VERY_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.VERY_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.AVERAGE,
+                                    (int)CalculationConstants.borrowerProfiles.NOT_SO_GOOD
+                                },
+                                {
+                                    (int)CalculationConstants.borrowerProfiles.VERY_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.AVERAGE,
+                                    (int)CalculationConstants.borrowerProfiles.NOT_SO_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.NOT_SO_GOOD
+                                },
+                                {
+                                    (int)CalculationConstants.borrowerProfiles.GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.AVERAGE,
+                                    (int)CalculationConstants.borrowerProfiles.NOT_SO_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.NOT_SO_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.BAD,
+                                },
+                                {
+                                    (int)CalculationConstants.borrowerProfiles.AVERAGE,
+                                    (int)CalculationConstants.borrowerProfiles.NOT_SO_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.NOT_SO_GOOD,
+                                    (int)CalculationConstants.borrowerProfiles.BAD,
+                                    (int)CalculationConstants.borrowerProfiles.NOTOK
+                                }
+        };
         // End of Israel only
 
- 
-           // handle borrower class with FICO or class 
 
-        public BorrowerProfile(uint ficoScore)
+        // handle borrower class with FICO or class 
+
+        public BorrowerProfile(CalculationParameters CalculationParameters, int ficoScore = MiscConstants.UNDEFINED_INT)
         {
-            // get the profile from the fico store
-            //uint ficoScore = env.CalculationParameters.fico;
-            // TBD: this is hugly but short - in order to enable using FICO and class (1-6), once recognize as 1-6 it means the exact class and not fico...
-            if (0 <= ficoScore && ficoScore < Enum.GetValues(typeof(borrowerProfiles)).Length)
+            if (MiscConstants.UNDEFINED_INT == ficoScore)
             {
-                profile = (int)Enum.Parse(typeof(borrowerProfiles), ficoScore.ToString(), true);
+                profile = GetBorrowerProfile(CalculationParameters);
             }
             else
             {
-                if ((0 < ficoScore) && (550 >= ficoScore))
+                // get the profile from the fico store
+                //uint ficoScore = env.CalculationParameters.fico;
+                // TBD: this is hugly but short - in order to enable using FICO and class (1-6), once recognize as 1-6 it means the exact class and not fico...
+                if (0 <= ficoScore && ficoScore < Enum.GetValues(typeof(borrowerProfiles)).Length)
                 {
-                    profile = (int)CalculationConstants.borrowerProfiles.AVERAGE;
-                }
-                else if ((550 < ficoScore) && (650 >= ficoScore))
-                {
-                    profile = (int)CalculationConstants.borrowerProfiles.GOOD;
-                }
-                else if ((650 < ficoScore) && (750 >= ficoScore))
-                {
-                    profile = (int)CalculationConstants.borrowerProfiles.VERY_GOOD;
-                }
-                else if ((750 < ficoScore) && (850 >= ficoScore))
-                {
-                    profile = (int)CalculationConstants.borrowerProfiles.BEST;
+                    profile = (int)Enum.Parse(typeof(borrowerProfiles), ficoScore.ToString(), true);
                 }
                 else
                 {
-                    // Should happen...
-                    profile = (int)CalculationConstants.borrowerProfiles.NOTOK;
+                    if ((0 < ficoScore) && (550 >= ficoScore))
+                    {
+                        profile = (int)CalculationConstants.borrowerProfiles.AVERAGE;
+                    }
+                    else if ((550 < ficoScore) && (650 >= ficoScore))
+                    {
+                        profile = (int)CalculationConstants.borrowerProfiles.GOOD;
+                    }
+                    else if ((650 < ficoScore) && (750 >= ficoScore))
+                    {
+                        profile = (int)CalculationConstants.borrowerProfiles.VERY_GOOD;
+                    }
+                    else if ((750 < ficoScore) && (850 >= ficoScore))
+                    {
+                        profile = (int)CalculationConstants.borrowerProfiles.BEST;
+                    }
+                    else
+                    {
+                        // Should happen...
+                        profile = (int)CalculationConstants.borrowerProfiles.NOTOK;
 
-                    // TBD. to be on the safe side
-                    profile = (int)CalculationConstants.borrowerProfiles.AVERAGE;
+                        // TBD. to be on the safe side
+                        profile = (int)CalculationConstants.borrowerProfiles.AVERAGE;
+                    }
                 }
             }
          }
 
-        
+        // **************************************************************************************************************************** //
+        // ************************************ Getting Borrower Profile According to LTV and PTI ************************************* //
+
+        private int GetBorrowerProfile(CalculationParameters CalculationParameters)
+        {
+            int borrowerProfile = (int)CalculationConstants.borrowerProfiles.NOTSET;
+
+            int ltvCounter = 0;
+            int ptiCounter = 0;
+            while (borrowerProfile == (int)CalculationConstants.borrowerProfiles.NOTSET)
+            {
+                if (CalculationParameters.pti <= ptiRatios[ptiCounter])
+                {
+                    if (CalculationParameters.ltv <= ltvRatios[ltvCounter])
+                    {
+                        borrowerProfile = profileMatrix[ptiCounter, ltvCounter];
+                    }
+                    else
+                    {
+                        if (ltvCounter == (ltvRatios.Length - 1))
+                        {
+                            borrowerProfile = (int)CalculationConstants.borrowerProfiles.NOTOK;
+                        }
+                        else
+                        {
+                            ltvCounter++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (ptiCounter == (ptiRatios.Length - 1))
+                    {
+                        borrowerProfile = (int)CalculationConstants.borrowerProfiles.NOTOK;
+                    }
+                    else
+                    {
+                        ptiCounter++;
+                    }
+                }
+            }
+            Console.WriteLine("Matrix Index [" + (ptiCounter + 1) + "," + (ltvCounter + 1) + "] = " + borrowerProfile
+                                                + "\nLTV = " + Math.Round(CalculationParameters.ltv * 100, 2) + "%\nPTI = "
+                                                + Math.Round(CalculationParameters.pti * 100, 2)
+                                                + "%\nProfile = " + CalculationConstants.profiles[borrowerProfile]);
+            return borrowerProfile;
+        }
+
+
+
+
         // **************************************************************************************************************************** //
         // *********************************************** Print Borrower Profile in Console ****************************************** //
-        
+
         public int ShowBorrowerProfile()
         {
             return profile;
