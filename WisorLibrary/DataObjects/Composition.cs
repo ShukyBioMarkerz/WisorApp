c7da36d05dce8b3cbc394eb6eabf21bdc095ea26
Shuky Barbash <shuky.barbash@gmail.com>
2017-06-21 14:46:34 +0300

    update the toString function

--- WisorLibrary/DataObjects/Composition.cs
+++ WisorLibrary/DataObjects/Composition.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using WisorLibrary.Logic;
+
 
 namespace WisorLib
 {
+    [Serializable]
     public class Composition
     {
         // General Parameters
+        public string name;
         public Option[] opts = {null, null, null};
         private bool[] ttlPayCalculated = {false, false, false};
-        public double ttlPmt = -1;
+        public double ttlPmt = 0;
         public double ttlPay = 0;
+        [XmlIgnoreAttribute]
         public double ttlRatePay = 0;
+        [XmlIgnoreAttribute]
         public double ttlPrincipalPay = 0;
+        [XmlIgnoreAttribute]
+        public double score = 0; // enable to sort the composition by scoring
 
-        double optXBankTtlPay, optYBankTtlPay, optZBankTtlPay;
-
+        public uint optXBankTtlPay, optYBankTtlPay, optZBankTtlPay;
+        [XmlIgnoreAttribute]
         public bool calcTtlPayOrNo = false;
+
+        // for the sake of serialization.....
+        public Composition()
+        {
+        }
 
         public Composition(Option optionX, Option optionY, Option optionZ, RunEnvironment env)
         {
@@ -111,27 +124,65 @@ namespace WisorLib
         // **************************************************************************************************************************** //
         // *************************************************** Print Composition To String ******************************************** //
 
+        public static string PrintHeader()
+        {
+            string s = "\nComposition list:";
+
+            s += /*"Ticks" + "," + "OrderID" + "," + "Time" + "," +*/
+                "X:optType" + "," + "X:optAmt" + "," + "X:optTime" + "," + "X:RateFirstPeriod" + "," +
+                "Y:optType" + "," + "Y:optAmt" + "," + "Y:optTime" + "," + "Y:RateFirstPeriod" + "," +
+                "Z:optType" + "," + "Z:optAmt" + "," + "Z:optTime" + "," + "Z:RateFirstPeriod" + "," +
+                "OPTXPmt" + "," + "OPTYPmt" + "," + "OPTZPmt" + "," + "ttlPmt" + "," +
+                "OPTXTtlPay" + "," + "OPTYTtlPay" + "," + "OPTZTtlPay" + "," + 
+                // bank profit data
+                "X:BankTtlPay" + "," + "Y:BankTtlPay" + "," + "Z:BankTtlPay" + "," +
+                "ttlBorrowerPay" + "," + "TtlBankPay" + "," +  "TtlBankProfit";
+
+            return s;
+        }
+
         public override string ToString()
         {
             int ttlBankPayPayk = Convert.ToInt32(optXBankTtlPay + optYBankTtlPay + optZBankTtlPay);
+            string s = MiscConstants.UNDEFINED_STRING;
 
-            return "" + opts[(int)Options.options.OPTX]
-                    + "," + opts[(int)Options.options.OPTY]
-                        + "," + opts[(int)Options.options.OPTZ]
-                        + "," + (int)opts[(int)Options.options.OPTX].optPmt
-                        + "," + (int)opts[(int)Options.options.OPTY].optPmt
-                        + "," + (int)opts[(int)Options.options.OPTZ].optPmt
-                        + "," + (int)ttlPmt
-                        + "," + (int)opts[(int)Options.options.OPTX].optTtlPay
-                        + "," + (int)opts[(int)Options.options.OPTY].optTtlPay
-                        + "," + (int)opts[(int)Options.options.OPTZ].optTtlPay
-                        + "," + (int)ttlPay
-                        // the bank profit data
-                        + "," + Convert.ToInt32(optXBankTtlPay).ToString()
-                        + "," + Convert.ToInt32(optYBankTtlPay).ToString()
-                        + "," + Convert.ToInt32(optZBankTtlPay).ToString()
-                        + "," + Convert.ToInt32(ttlBankPayPayk).ToString()
-                        + "," + Convert.ToInt32(ttlPay - ttlBankPayPayk).ToString();
+            if (null != opts && null != opts[(int)Options.options.OPTX] && null != opts[(int)Options.options.OPTY] &&
+                null != opts[(int)Options.options.OPTZ])
+            {
+                s = "" + opts[(int)Options.options.OPTX].ToString()
+                        + "," + opts[(int)Options.options.OPTY].ToString()
+                            + "," + opts[(int)Options.options.OPTZ].ToString()
+                            + "," + (int)opts[(int)Options.options.OPTX].optPmt
+                            + "," + (int)opts[(int)Options.options.OPTY].optPmt
+                            + "," + (int)opts[(int)Options.options.OPTZ].optPmt
+                            + "," + (int)ttlPmt
+                            + "," + (int)opts[(int)Options.options.OPTX].optTtlPay
+                            + "," + (int)opts[(int)Options.options.OPTY].optTtlPay
+                            + "," + (int)opts[(int)Options.options.OPTZ].optTtlPay
+                            // the bank profit data
+                            + "," + Convert.ToInt32(optXBankTtlPay).ToString()
+                            + "," + Convert.ToInt32(optYBankTtlPay).ToString()
+                            + "," + Convert.ToInt32(optZBankTtlPay).ToString()
+                            + "," + (int)ttlPay
+                            + "," + Convert.ToInt32(ttlBankPayPayk).ToString()
+                            + "," + Convert.ToInt32(ttlPay - ttlBankPayPayk).ToString()
+                            /*+ "," + score*/;
+            }
+            return s;
+        }
+
+        public static Predicate<Composition> CompositionPredicate(Composition c)
+        {
+            return delegate (Composition comp)
+            {
+                return
+                    comp.opts[(int)Options.options.OPTX].optAmt == c.opts[(int)Options.options.OPTX].optAmt &&
+                    comp.opts[(int)Options.options.OPTX].optTime == c.opts[(int)Options.options.OPTX].optTime &&
+                    comp.opts[(int)Options.options.OPTY].optAmt == c.opts[(int)Options.options.OPTY].optAmt &&
+                    comp.opts[(int)Options.options.OPTY].optTime == c.opts[(int)Options.options.OPTY].optTime &&
+                    comp.opts[(int)Options.options.OPTZ].optAmt == c.opts[(int)Options.options.OPTZ].optAmt &&
+                    comp.opts[(int)Options.options.OPTZ].optTime == c.opts[(int)Options.options.OPTZ].optTime;
+            };
         }
 
     }
