fc05399ea02cd3f46520b9554bc469202c258cfa
Shuky Barbash <shuky.barbash@gmail.com>
2017-06-21 14:50:02 +0300

    add benefit to enable calculate if this product should be in the combinations

--- WisorLibrary/DataObjects/GenericProduct.cs
+++ WisorLibrary/DataObjects/GenericProduct.cs
@@ -35,7 +35,6 @@ namespace WisorLib
         public ProductID productID { get; set; } // unique identifier
   
         // Identify which local market is used
-        public enum markets { USA, UK, ISRAEL, OTHER , NONE}; // Are the options in the code or pulled from outside DB?
         public markets localMarket;
 
         // Identify the Index used to calculate rate
@@ -43,11 +42,16 @@ namespace WisorLib
         public double indexUsedFirstTimePeriod { get; set; }
         public double indexUsedSecondTimePeriod { get; set; }
         public indices originalIndexUsedFirstTimePeriod { get; set; }
+        public /*Benefit*/ double benefit { get; set; }
+        public FixOrAdjustable fixOrAdjustable { get; set; }
+        public /*Risk*/ double risk { get; set; }
+        public /*Liquidity*/ double liquidity { get; set; }
+        public bool mustBeUsed { get; set; }
+        public bool shouldConsider { get; set; }
+
 
         // Identify the time between updates of chosen index
         // Once Index is identified -> DB has the historical rates -> the final value should be an number
-        public enum indexJumps { DAY, WEEK, MONTHS1, MONTHS3, MONTHS6, MONTHS12, MONTHS24, MONTHS30, MONTHS36, MONTHS60,
-                MONTHS84, MONTHS120, OTHER , NONE};
         public indexJumps indexJumpFirstTimePeriod;
         public indexJumps indexJumpSecondTimePeriod;
 
@@ -73,13 +77,18 @@ namespace WisorLib
 
         public double maxPercentageOfLoan;
 
+        public uint Score { get; set; }
+
         public GenericProduct(ProductID productID, markets localMarket = markets.NONE, // USA, UK, ISRAEL, OTHER
             string name = MiscConstants.UNDEFINED_STRING, 
             indices indexUsedFirstTimePeriod = indices.NONE, indices indexUsedSecondTimePeriod = indices.NONE, // PRIME, FED, LIBOR, EUROBOR, BBBR, OTHER, NONE
             indexJumps indexJumpFirstTimePeriod = indexJumps.NONE, indexJumps indexJumpSecondTimePeriod = indexJumps.NONE, // AY, WEEK, MONTHS1, MONTHS3, MONTHS6, MONTHS12, MONTHS24, MONTHS30, MONTHS36, MONTHS60, MONTHS84, MONTHS120, OTHER
             uint minTime = MiscConstants.UNDEFINED_UINT, uint maxTime = MiscConstants.UNDEFINED_UINT,
             uint timeJump = MiscConstants.UNDEFINED_UINT, uint firstTimePeriod = MiscConstants.UNDEFINED_UINT,
-            double maxPercentageOfLoan = MiscConstants.UNDEFINED_DOUBLE) 
+            double maxPercentageOfLoan = MiscConstants.UNDEFINED_DOUBLE,
+            /*Risk*/ double risk = MiscConstants.UNDEFINED_DOUBLE, /*Liquidity*/ double liquidity = MiscConstants.UNDEFINED_DOUBLE,
+            FixOrAdjustable fixOrAdjustable = FixOrAdjustable.ADJUSTABLE, /*Benefit*/ double benefit = MiscConstants.UNDEFINED_DOUBLE,
+            bool mustBeUsed = MiscConstants.UNDEFINED_BOOL, bool shouldBeUsed = MiscConstants.UNDEFINED_BOOL) 
         {
             this.productID = productID;
             this.localMarket = localMarket;
@@ -94,8 +103,17 @@ namespace WisorLib
             this.timeJump = timeJump;
             this.firstTimePeriod = firstTimePeriod;
             this.maxPercentageOfLoan = maxPercentageOfLoan;
+            this.benefit = benefit;
+            this.fixOrAdjustable = fixOrAdjustable;
+            this.risk = risk;
+            this.liquidity = liquidity;
+            this.mustBeUsed = mustBeUsed;
+            this.shouldConsider = shouldBeUsed;
 
             CheckCorrectness();
+
+            Score = CalculateScore();
+
          }
 
         // Alert for illegal values
@@ -126,56 +144,125 @@ namespace WisorLib
             if (null != msg)
             {
                 WindowsUtilities.loggerMethod("ERROR: CheckCorrectness for productID: " + productID.stringTypeId + " found: " + msg);
-                Console.WriteLine("ERROR: CheckCorrectness for productID: " + productID.stringTypeId + " found: " + msg);
             }
         }
 
+        // each product have a beneficial score dependent on the risk, liquidity and benefit parameters
+        uint CalculateScore()
+        {
+            return MiscUtilities.CalculateProductScore(risk, liquidity, benefit);
+        }
 
+    
         public static ProductsList LoadXMLProductsFile(string filename)
         {
             ProductsList products = new ProductsList();
             XElement currProduct = null;
+            markets market;
+            string name, typeId;
+            indices iftp, istp;
+            indexJumps ijftp, ijstp;
+            uint minTime, maxTime, timeJump, firstTimePeriod;
+            double maxPercentageLoan = MiscConstants.UNDEFINED_DOUBLE;
+            int index = 0;
+            /*Benefit*/ double benefit;
+            FixOrAdjustable fixOrAdjustable;
+            /*Risk*/ double risk;
+            /*Liquidity*/ double liquidity;
+            HashSet<string> productsHash = new HashSet<string>();
+            bool mustBeUsed, shouldConsider;
+            uint score;
+            bool beneficial;
 
             // Load only th nneded products according to the combination definition
-            try {
+            try
+            {
                 if (File.Exists(filename))
                 {
                     XDocument doc = XDocument.Load(filename);
 
                     // loop all the items in the document
-                    foreach (XElement product in from p in doc.Descendants("Product") select p)
+                    foreach (XElement product in from p in doc.Descendants(MiscConstants.Product) select p)
                     {
                         currProduct = product;
-                        markets market = (markets)Enum.Parse(typeof(markets), product.Element("market").Value, true);
-                        string name = product.Element("name").Value;
-                        string typeId = product.Element("typeId").Value;
-                        indices iftp = (indices)Enum.Parse(typeof(indices), product.Element("indexUsedFirstTimePeriod").Value, true);
-                        indices istp = (indices)Enum.Parse(typeof(indices), product.Element("indexUsedSecondTimePeriod").Value, true);
-                        indexJumps ijftp = (indexJumps)Enum.Parse(typeof(indexJumps), product.Element("indexJumpFirstTimePeriod").Value, true);
-                        indexJumps ijstp = (indexJumps)Enum.Parse(typeof(indexJumps), product.Element("indexJumpSecondTimePeriod").Value, true);
-                        uint minTime = Convert.ToUInt32(product.Element("minTime").Value);
-                        uint maxTime = Convert.ToUInt32(product.Element("maxTime").Value);
-                        uint timeJump = Convert.ToUInt32(product.Element("timeJump").Value);
-                        //string optName = product.Element("optName").Value;
-                        uint firstTimePeriod = Convert.ToUInt32(product.Element("firstTimePeriod").Value);
-                        double maxPercentageLoan = MiscConstants.UNDEFINED_DOUBLE; 
-                        if (null != product.Element("maxPercentageOfLoan"))
-                            maxPercentageLoan = Convert.ToDouble(product.Element("maxPercentageOfLoan").Value);
+                        typeId = product.Element("typeId").Value;
 
+                        // check if the product should be loaded
+                        shouldConsider = MiscConstants.UNDEFINED_BOOL;
+                        if (null != product.Element(MiscConstants.shouldConsider) && MiscConstants.UNDEFINED_STRING != product.Element(MiscConstants.shouldConsider).Value)
+                            shouldConsider = Convert.ToBoolean(product.Element(MiscConstants.shouldConsider).Value);
+                        if (!shouldConsider)
+                        {
+                            WindowsUtilities.loggerMethod("LoadXMLProductsFile producs: " + typeId +
+                            " shouldConsider not be used. Skip it.");
+                            continue;
+                        }
+
+                        market = (markets)Enum.Parse(typeof(markets), product.Element(MiscConstants.market).Value, true);
+                        // ensure its the needed market
+                        if (market != Share.theMarket)
+                            continue;
+                        name = product.Element(MiscConstants.name).Value;
+                        iftp = (indices)Enum.Parse(typeof(indices), product.Element(MiscConstants.indexUsedFirstTimePeriod).Value, true);
+                        istp = (indices)Enum.Parse(typeof(indices), product.Element(MiscConstants.indexUsedSecondTimePeriod).Value, true);
+                        ijftp = (indexJumps)Enum.Parse(typeof(indexJumps), product.Element(MiscConstants.indexJumpFirstTimePeriod).Value, true);
+                        ijstp = (indexJumps)Enum.Parse(typeof(indexJumps), product.Element(MiscConstants.indexJumpSecondTimePeriod).Value, true);
+                        minTime = Convert.ToUInt32(product.Element(MiscConstants.minTime).Value);
+                        maxTime = Convert.ToUInt32(product.Element(MiscConstants.maxTime).Value);
+                        timeJump = Convert.ToUInt32(product.Element(MiscConstants.timeJump).Value);
+                        //string optName = product.Element("optName").Value;
+                        firstTimePeriod = Convert.ToUInt32(product.Element(MiscConstants.firstTimePeriod).Value);
+                        maxPercentageLoan = MiscConstants.UNDEFINED_DOUBLE; 
+                        if (null != product.Element(MiscConstants.maxPercentageOfLoan))
+                            maxPercentageLoan = Convert.ToDouble(product.Element(MiscConstants.maxPercentageOfLoan).Value);
+                        benefit = MiscConstants.UNDEFINED_DOUBLE; /*Benefit.NONEBenefit;*/
+                        if (null != product.Element(MiscConstants.benefit))
+                            benefit = Convert.ToDouble(product.Element(MiscConstants.benefit).Value); //  (Benefit)Enum.Parse(typeof(Benefit), product.Element("benefit").Value, true);
+                        fixOrAdjustable = FixOrAdjustable.ADJUSTABLE;
+                        if (null != product.Element(MiscConstants.fixOrAdjustable) && MiscConstants.UNDEFINED_STRING != product.Element(MiscConstants.fixOrAdjustable).Value)
+                            fixOrAdjustable = (FixOrAdjustable)Enum.Parse(typeof(FixOrAdjustable), product.Element(MiscConstants.fixOrAdjustable).Value, true);
+                        risk = MiscConstants.UNDEFINED_DOUBLE;  /*Risk.NONERisk;*/
+                        if (null != product.Element(MiscConstants.risk))
+                            risk = Convert.ToDouble(product.Element(MiscConstants.risk).Value); // (Risk)Enum.Parse(typeof(Risk), product.Element("risk").Value, true);
+                        liquidity = MiscConstants.UNDEFINED_DOUBLE;  /*Liquidity.NONELiquidity;*/
+                        if (null != product.Element(MiscConstants.liquidity))
+                            liquidity = Convert.ToDouble(product.Element(MiscConstants.liquidity).Value); // (Liquidity)Enum.Parse(typeof(Liquidity), product.Element("liquidity").Value, true);
+                        mustBeUsed = MiscConstants.UNDEFINED_BOOL;
+                        if (null != product.Element(MiscConstants.mustBeUsed) && MiscConstants.UNDEFINED_STRING != product.Element(MiscConstants.mustBeUsed).Value)
+                            mustBeUsed = Convert.ToBoolean(product.Element(MiscConstants.mustBeUsed).Value);
+                        
                         // ensure the product is needed
-                        int index = GenericProduct.GetProductIndex(typeId);
-                        if (MiscConstants.UNDEFINED_INT < index)
+                        // set the index of the product later
+                        //index = GenericProduct.GetProductIndex(typeId);
+                        // ensure the beneficial score is above the threshold
+                        score = MiscUtilities.CalculateProductScore(risk, liquidity, benefit);
+                        beneficial = MiscUtilities.CheckBeneficialProducts(score);
+                        
+                        // debug the score calculation
+                        WindowsUtilities.loggerMethod("LoadXMLProductsFile producs: " + typeId +
+                            ", risk: " + risk + ", liquidity: " + liquidity + ", benefit: " + benefit + 
+                            " , score: " + score + ", is beneficial: " + beneficial);
+
+                        if (/*MiscConstants.UNDEFINED_INT < index &&*/ beneficial)
                         {
                             ProductID productID = new ProductID(index, typeId);
-                            products.Add(index, new GenericProduct(productID /*ID*/, market /*localMarket*/, name,
+                            products.Add(index++, new GenericProduct(productID /*ID*/, market /*localMarket*/, name,
                                 iftp /*indices*/, istp /*indices*/,
                                 ijftp /*indexJumps*/, ijstp /*indexJumps*/,
-                                minTime, maxTime, timeJump, firstTimePeriod, maxPercentageLoan));
+                                minTime, maxTime, timeJump, firstTimePeriod, maxPercentageLoan,
+                                risk, liquidity, fixOrAdjustable, benefit, mustBeUsed, shouldConsider));
+
+                            // add the product name to the indexed list
+                            productsHash.Add(typeId);
                         }
                         // the product in the products file does not have rate so ignore it
+                        // or beneficial
                         else
                         {
-                            //WindowsUtilities.loggerMethod("LoadXMLProductsFile producs: " + typeId + " does not have rate. Ignore it.");
+                            //if (MiscConstants.UNDEFINED_INT >= index)
+                            //    WindowsUtilities.loggerMethod("LoadXMLProductsFile producs: " + typeId + " does not have rate. Ignore it.");
+                            if (!beneficial)
+                                WindowsUtilities.loggerMethod("LoadXMLProductsFile producs: " + typeId + " does not defined as beneficial. Ignore it.");
                         }
 
                     }
@@ -193,15 +280,17 @@ namespace WisorLib
             if (null == products || 0 >= products.Count)
             {
                 WindowsUtilities.loggerMethod("ERROR: failed to load products from file: " + filename);
-                Console.WriteLine("ERROR: failed to load products from file: " + filename);
             }
             else
             {
                 WindowsUtilities.loggerMethod("LoadXMLProductsFile succeffuly load: " + products.Count + " products from file: " +
                     filename.Substring(filename.LastIndexOf(Path.DirectorySeparatorChar) + 1));
-                Console.WriteLine("LoadXMLProductsFile succeffuly load: " + products.Count + " products from file: " +
-                    filename.Substring(filename.LastIndexOf(Path.DirectorySeparatorChar) + 1));
             }
+
+            string[] productNames = productsHash.ToArray();
+            WindowsUtilities.loggerMethod("NOTICE: #of productNames: " + productNames.Length + " from file: " +
+                filename.Substring(filename.LastIndexOf(Path.DirectorySeparatorChar) + 1), true);
+            Share.theProductsNames = productNames;
             Share.theLoadedProducts = products;
             return products;
         }
